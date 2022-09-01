3ee816e6c55f24d031fafa6adf41eb1ccf468ed8
CatalinaRossi <catalinaarossi@gmail.com>
2022-09-01 19:38:03 -0300

    Entradas y salidas de datos

---
+++ Clase_1/entrada_salida_datos.py
@@ -0,0 +1,21 @@
+#SALIDA DE DATOS
+print("Hola Mundo...")
+print("Desde visual")
+
+#salto de linea
+print("Esto es un salto \n de \t linea")
+
+#Tabulacion
+print("Con esto se \t tabula")
+
+#Concatenar
+print("hola" + "Mundo")
+
+#Sep
+print("Hola", "Mundo", sep="+")
+
+#End
+print("Hola Mundo...", end="\t")
+
+#ENTRADA DE DATOS
+input("Ingrese un mensaje: ")
