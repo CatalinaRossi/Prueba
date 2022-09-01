28378217619e334de700a33683aa47d7e85c605a
CatalinaRossi <catalinaarossi@gmail.com>
2022-09-01 20:55:00 -0300

    Clase 2

---
+++ Clase_1/variables.py
@@ -0,0 +1,16 @@
+# VARIABLES
+
+#mensaje_usuario = input("Ingrese un mensaje: ")
+#print(mensaje_usuario)
+
+x = int(input("Ingrese un numero: "))
+y = int(input("Ingrese un numero: "))
+
+print("El tipo de dato de x es : ",type(x))
+print(type(y))
+
+resultado = x + y
+
+print(type(resultado))
+
+print("El resultado es --> ", resultado)
